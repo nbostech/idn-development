8c281de7132ec8e643dfad304a44ed3b4b56a82c
Vivek Kiran <vivek.kiran@outlook.com>
2016-06-27 17:08:50 +0530

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/nbostech/wavelabs-android-app.git idn-android-app
+git clone https://github.com/nbostech/wavelabs-java-client-api.git idn-java-api
+git clone https://github.com/nbostech/wavelabs-android-client-api.git idn-android-api
