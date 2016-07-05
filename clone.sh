4d77cda2bab44690014c0b63d356c7942568e31b
Vivek Kiran <vivek.kiran@outlook.com>
2016-07-05 14:20:58 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,3 @@
-git clone -b develop https://github.com/nbostech/wavelabs-android-app.git idn-android-app
+git clone https://github.com/nbostech/wavelabs-android-app.git idn-android-app
 git clone https://github.com/nbostech/wavelabs-java-client-api.git idn-java-api
 git clone https://github.com/nbostech/wavelabs-android-client-api.git idn-android-api
