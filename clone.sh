6ba5b2e185fedd1c29181696f9e19566c9f07892
Vivek Kiran <vivek.kiran@outlook.com>
2016-08-10 22:29:57 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/nbostech/nbos-android-starter-app.git idn-android-app
 git clone https://github.com/nbostech/idn-sdk-java.git idn-java-api
 git clone https://github.com/nbostech/idn-sdk-android.git idn-android-api
+git clone https://github.com/nbostech/nbos-media-java-module nbos-media-java-module
+git clone https://github.com/nbostech/nbos-identity-java-module nbos-identity-java-module
